e43c64eac0591d5a20b221aa15d82a86e3e38a23
Loner <853438532@qq.com>
2016-10-19 23:36:47 +0800

    Delete createdata.py

--- GradientDecent/createdata.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-
-f = open("data.txt", "wr")
-b = [np.random.rand() * 10 for i in range(200)]
-for i in b:
-    f.write("%.02f %.02f\n" % (float(i), (float(i)+np.random.rand()) ** 2))
-f.close()
