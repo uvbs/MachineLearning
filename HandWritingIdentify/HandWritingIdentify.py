58088f4542d19da17c2d8b68d0dd61f80fb737af
loner <yinjiakang@gmail.com>
2016-11-30 23:26:11 +0800

    modified:   HandWritingIdentify.py 	new file:   test1.jpg 	new file:   test2.jpg 	new file:   test3.jpg

--- HandWritingIdentify/HandWritingIdentify.py
+++ HandWritingIdentify/HandWritingIdentify.py
@@ -24,6 +24,7 @@ def classify(sample,dataset,labels,k):
         label = labels[SortedIndex[i]]
         CountOfLabel[label] = CountOfLabel.get(label,0) + 1
     SortedCount = sorted(CountOfLabel.items(), key=lambda d:d[1], reverse=True)
+    print(SortedCount)
     return SortedCount[0][0]
 
 #open training set
@@ -47,19 +48,90 @@ def trainingsetconstructor():
         labels.append(label)
     return np.array(dataset),labels
 
-#open and trans sample
-def sample_function(image_name):
+    
+#divide image into parts
+def imagedivide(image):
+    count_pixel = 0
+    total_count_pixel = 0
+    plainpoint = []
+    isbreak = False
+    (x,y) = image.size
+    ImagePixel = [image.getpixel((w,h)) for h in range(y) for w in range(x)]
+    total_count_pixel = sum(ImagePixel)
+    MeanOfPixel = total_count_pixel / ( x * y )
+    for t_x in range(x):
+        for t_y in range(y):
+            count_pixel += image.getpixel((t_x,t_y))
+        MeanOfColPixel = count_pixel / y
+        if MeanOfColPixel < MeanOfPixel:
+            plainpoint.append(t_x)    
+        count_pixel = 0
+    b = []
+    breakpoint = []
+    for i in range(x):
+        if i not in plainpoint and isbreak == False:
+            b.append(i);isbreak = True
+        if i not in plainpoint and isbreak == True:
+            b.append(i)
+        if i in plainpoint and isbreak == True:
+            breakpoint.append(round(sum(b)/len(b)))
+            b = [];isbreak = False
+    breakpoint[0] = -1
+    startandstop = []
+    for i in range(len(breakpoint)):
+        if i != len(breakpoint)-1:
+            startandstop.append((breakpoint[i]+1,breakpoint[i+1]))
+        else:
+            startandstop.append((breakpoint[-1],x-1))
+    return startandstop
+    
+    
+    
+    
+    
+    
+    
+    return breakpoint
+
+
+
+
+#open and convert image
+def opener(image_name):
     image = Image.open(image_name)
     image=image.convert("L")
+    return image
+    
+    
+    
+    
+    
+    
+#change sample into bins
+def changeimage(image):
     resizeimage = image.resize((32,32),Image.ANTIALIAS)
     ImageList = [1 if resizeimage.getpixel((w,h)) < 127.5 else 0 for h in range(0,  32) for w in range(0, 32)]
     return np.array(ImageList)
 
-#main function to  dispatch system
-def main():
-    Dataset,Labels = trainingsetconstructor()
-    sample = sample_function("test.jpg")
-    print(classify(sample,Dataset,Labels,20))
     
+    
+    
+
+#main function to  dispatch system
+def main():  
+    str = ""
+    Dataset,Labels = trainingsetconstructor()
+    image = opener("test3.jpg")  
+    (x,y) = image.size
+    breakpoints = imagedivide(image)
+    print(breakpoints )
+    for (a,b) in breakpoints:
+        partofimage = image.crop((a,0,b+1,32))
+        print(partofimage)
+        partofimage.show()
+        sample = changeimage(partofimage)
+        str += classify(sample,Dataset,Labels,100)
+    print(str)
+        
 if __name__ == "__main__":
     main()
